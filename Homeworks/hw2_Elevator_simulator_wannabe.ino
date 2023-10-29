bd186a2be951433e17ccaba37e291ea9e46306ac
Cirstea Ruxandra-Gabriela <115624657+RuxiC@users.noreply.github.com>
2023-10-29 19:50:02 +0200

    Update hw2_Elevator_simulator_wannabe.ino

--- Homeworks/hw2_Elevator_simulator_wannabe.ino
+++ Homeworks/hw2_Elevator_simulator_wannabe.ino
@@ -7,7 +7,7 @@ const int floor1LED = 12;
 const int floor2LED = 13;  
 const int buzzerPin = 9; 
 
-bool isMoving = false;  // Starea curentă a liftului
+bool isMoving = false;  // Starea curenta a liftului
 int currentFloor = 0;  // Etajul curent al liftului
 
 void setup() {
@@ -20,11 +20,11 @@ void setup() {
   pinMode(floor2LED, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
   digitalWrite(elevatorMovingLED, HIGH);
-  digitalWrite(floor0LED, HIGH);     // Inițializare stare curentă
+  digitalWrite(floor0LED, HIGH);     // Initializare stare curenta
 }
 
 void loop() {
-  // Verificare butoane apăsate
+  // Verificare butoane apasate
   if (digitalRead(floor0ButtonPin) == LOW) {
     moveElevator(0);
   } else if (digitalRead(floor1ButtonPin) == LOW) {
@@ -41,27 +41,24 @@ void loop() {
       stopElevator();
     }
   }
-  
 }
 
 void moveElevator(int targetFloor) {
-  
   if (!isMoving) {
     isMoving = true;
     delay(2000);  
     // Stinge LED-urile 1 și 3
     digitalWrite(floor0LED, LOW);
     digitalWrite(floor2LED, LOW);
-    if((currentFloor == 0 && targetFloor == 1) || (currentFloor == 1 && targetFloor == 2) || (currentFloor == 1 && targetFloor == 0) || (currentFloor == 2 && targetFloor == 1)){
-      playFloorSound2();  // Redă sunetul la trecerea prin etaj
+    if((currentFloor == 0 && targetFloor == 1) || (currentFloor == 1 && targetFloor == 2) || (currentFloor == 1 && targetFloor == 0) || (currentFloor == 2 && targetFloor == 1)) {
+      playFloorSound2();  // Reda sunetul la trecerea prin etaj
       digitalWrite(elevatorMovingLED, LOW); 
       delay(500); 
       digitalWrite(elevatorMovingLED, HIGH);
       delay(500); 
-    }
-    else{
+    } else {
       if ((currentFloor == 0 && targetFloor == 2) || (currentFloor == 2 and targetFloor == 0)) {
-      playFloorSound1();  // Redă sunetul la trecerea prin etaj
+      playFloorSound1();  // Reda sunetul la trecerea prin etaj
       digitalWrite(elevatorMovingLED, LOW); 
       delay(500);
       digitalWrite(elevatorMovingLED, HIGH);
@@ -79,15 +76,15 @@ void moveElevator(int targetFloor) {
       delay(500); 
       digitalWrite(elevatorMovingLED, HIGH);
       delay(500);
-        digitalWrite(floor1LED, LOW);
+      digitalWrite(floor1LED, LOW);
       }
     }
-    // Actualizează etajul curent și LED-urile etajelor
+    // Actualizeaza etajul curent si LED-urile etajelor
     currentFloor = targetFloor;
     updateFloorLEDs();
-    stopElevator();  // Oprește liftul după sosirea la destinație
+    stopElevator();  // Opreste liftul după sosirea la destinatie
   }
-   playFloorSound();  // Redă sunetul la ajungerea liftului
+   playFloorSound();  // Reda sunetul la ajungerea liftului
 }
 
 void stopElevator() {
@@ -110,13 +107,13 @@ void updateFloorLEDs() {
   }
 }
 void playFloorSound() {
-  tone(buzzerPin, 1000, 200);  // Generează un sunet de 1000 Hz pentru 200 de milisecunde
+  tone(buzzerPin, 1000, 200);  // Genereaza un sunet de 1000 Hz pentru 200 de milisecunde
 }
 void playFloorSound1()
 {
-  tone(buzzerPin, 500, 4000);  // Generează un sunet de 500 Hz pentru 4000 de milisecunde
+  tone(buzzerPin, 500, 4000);  // Genereaza un sunet de 500 Hz pentru 4000 de milisecunde
 }
 void playFloorSound2()
 {
-  tone(buzzerPin, 500, 1000);  // Generează un sunet de 1000 Hz pentru 1000 de milisecunde
+  tone(buzzerPin, 500, 1000);  // Genereaza un sunet de 1000 Hz pentru 1000 de milisecunde
 }
